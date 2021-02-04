b2660f4c41362bb790f938cddc35a0fb6b99ff78
Ridho Maulana Abdi <75603377+ridhoma08@users.noreply.github.com>
2021-02-04 19:12:06 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+╭─「 Donasi • Pulsa + OVO」
+│ • Tree: 628998839509
+| • OVO : 628998839509
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
